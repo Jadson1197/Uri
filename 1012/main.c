1fa233579f31a4da1601af9dcf334e244d82ebad
Jadson Santos <jadson1197@gmail.com>
2019-01-22 07:38:12 -0300

    Area

---
+++ 1012/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define pi 3.14159
+
+int main() {
+ double a,b,c,triangulo,circulo,trapezio,quadrado,retangulo;
+    scanf("%lf%lf%lf",&a,&b,&c);
+
+    triangulo = a * c / 2;
+    circulo = pi * (c*c);
+    trapezio = ((a+b)*c)/2;
+    retangulo = a * b;
+    quadrado = b*b;
+
+    printf("TRIANGULO: %.3lf\n",triangulo);
+    printf("CIRCULO: %.3lf\n",circulo);
+    printf("TRAPEZIO: %.3lf\n",trapezio);
+    printf("QUADRADO: %.3lf\n",quadrado);
+    printf("RETANGULO: %.3lf\n",retangulo);
+
+    return 0;
+}
